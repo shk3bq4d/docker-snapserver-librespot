639cb1fb91ec7032ad23dd8e36db6d40a3a0f42a
shk3bq4d <123r4p+github-shk3bq4d-1e8c2700@gmail.com>
2024-10-24 15:03:18 +0200

    version 0.5.0-1.82-0.22.0-bullseye

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM shk3bq4d/snapserver
+FROM shk3bq4d/snapserver:0.22.0-bullseye
 
-COPY --from=shk3bq4d/librespot /usr/local/cargo/bin/librespot /usr/local/bin/
+COPY --from=shk3bq4d/librespot:0.5.0-1.82 /usr/local/cargo/bin/librespot /usr/local/bin/
 
 COPY run.sh /
 CMD ["/run.sh"]
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -euo pipefail
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+
+DIR="$( cd -P "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
+
+cd $DIR
+source dot.source
+
+$SUDO \
+docker build -t "$TAG" .
+
+echo EOF
+exit 0
