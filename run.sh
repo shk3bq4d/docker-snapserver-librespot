844cd5157043b7ce79a7566b27ceb5ac44d34f2a
shk3bq4d <123r4p+github-shk3bq4d-1e8c2700@gmail.com>
2024-10-24 15:48:41 +0200

    remove sed

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ set -euo pipefail
 # and finally launch snapserver with the updated config...
 set -x
 ls -l /usr/local/bin/librespot
-sed -r -i -e "s/1780/$HTTP_PORT/" /usr/share/snapserver/snapweb/snapcontrol.js
+#sed -r -i -e "s/1780/$HTTP_PORT/" /usr/share/snapserver/snapweb/snapcontrol.js
 exec snapserver -c /config/snapserver-${INSTANCE}.conf
